2b5e3e77076a63f4f418746a13f205c1acb00e48
vasudhamidhun <vasudhamadhuramath@gmail.com>
2026-05-15 10:31:12 +0530

    add email host user and password in enviornment

--- alumni/settings.py
+++ alumni/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'midhunvasudha@gmail.com'
-EMAIL_HOST_PASSWORD = 'mhrl qeyv uivp pnlf'
+# EMAIL_HOST_USER = 'midhunvasudha@gmail.com'
+# EMAIL_HOST_PASSWORD = 'mhrl qeyv uivp pnlf'
 
 EMAIL_HOST_USER = os.getenv("midhunvasudha@gmail.com")
 EMAIL_HOST_PASSWORD = os.getenv("mhrl qeyv uivp pnlf")
